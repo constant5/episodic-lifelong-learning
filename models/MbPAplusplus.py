8177160f8d26f2f70644df9b136decf154de8c11
constant5 <crmarksv@gmail.com>
2020-11-13 08:17:19 -0600

    added num labels argument

--- models/MbPAplusplus.py
+++ models/MbPAplusplus.py
@@ -91,7 +91,7 @@ class MbPAplusplus(nn.Module):
     Implements Memory based Parameter Adaptation model
     """
 
-    def __init__(self, L=30, model_state=None):
+    def __init__(self, L=30, model_state=None, num_labels=33):
         super(MbPAplusplus, self).__init__()
 
         if model_state is None:
@@ -100,12 +100,12 @@ class MbPAplusplus(nn.Module):
                 'bert-base-uncased')
             # Bert model for text classification
             self.classifier = transformers.BertForSequenceClassification.from_pretrained(
-                'bert-base-uncased', num_labels=33)
+                'bert-base-uncased', num_labels=num_labels)
 
         else:
 
             cls_config = transformers.BertConfig.from_pretrained(
-                'bert-base-uncased', num_labels=33)
+                'bert-base-uncased', num_labels=num_labels)
             self.classifier = transformers.BertForSequenceClassification(
                 cls_config)
             self.classifier.load_state_dict(model_state['classifier'])
